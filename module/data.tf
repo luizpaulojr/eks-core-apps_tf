4b9f4b4cdc94c546511bdfe0a66ad3c8cdd75e24
luizpaulojr <94655811+luizpaulojr@users.noreply.github.com>
2025-06-18 12:43:20 -0300

    Update data.tf
    add local cluster endpoint

--- module/data.tf
+++ module/data.tf
@@ -34,18 +34,19 @@ data "aws_security_group" "sg_node" {
 }
 
 locals {
-  region       = var.region
-  cluster_name = var.cluster_name
-  oidc         = substr(data.aws_eks_cluster.default.identity[0].oidc[0].issuer, 8, length(data.aws_eks_cluster.default.identity[0].oidc[0].issuer))
-  account_id   = data.aws_caller_identity.current.account_id
-  vpc_id       = data.aws_eks_cluster.default.vpc_config[0].vpc_id
-  sg_node      = data.aws_security_group.sg_node.id
-  subnet_1     = data.aws_subnet.subnet_1.id
-  subnet_2     = data.aws_subnet.subnet_2.id
-  subnet_3     = data.aws_subnet.subnet_3.id
-  az_1         = data.aws_subnet.subnet_1.availability_zone
-  az_2         = data.aws_subnet.subnet_2.availability_zone
-  az_3         = data.aws_subnet.subnet_3.availability_zone
+  region           = var.region
+  cluster_name     = var.cluster_name
+  cluster_endpoint = data.aws_eks_cluster.default.endpoint
+  oidc             = substr(data.aws_eks_cluster.default.identity[0].oidc[0].issuer, 8, length(data.aws_eks_cluster.default.identity[0].oidc[0].issuer))
+  account_id       = data.aws_caller_identity.current.account_id
+  vpc_id           = data.aws_eks_cluster.default.vpc_config[0].vpc_id
+  sg_node          = data.aws_security_group.sg_node.id
+  subnet_1         = data.aws_subnet.subnet_1.id
+  subnet_2         = data.aws_subnet.subnet_2.id
+  subnet_3         = data.aws_subnet.subnet_3.id
+  az_1             = data.aws_subnet.subnet_1.availability_zone
+  az_2             = data.aws_subnet.subnet_2.availability_zone
+  az_3             = data.aws_subnet.subnet_3.availability_zone
 }
 
 
