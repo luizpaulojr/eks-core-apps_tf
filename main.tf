24715b2b18fe7a608688bc821c81324e94dc0891
Luiz Ferreira <luiz.ferreira@brlink.com.br>
2025-07-08 17:13:48 -0300

    ajustes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "eks_core_apps" {
   source       = "./module"
   region       = "us-east-1"
-  cluster_name = "eks-default"
+  cluster_name = "eks-cluster"
  
   # kube_proxy - https://docs.aws.amazon.com/pt_br/eks/latest/userguide/managing-kube-proxy.html
   kube_proxy_enable  = false
@@ -13,17 +13,17 @@ module "eks_core_apps" {
 
   # vpc_cni - https://artifacthub.io/packages/helm/aws/aws-vpc-cni
   vpc_cni_enable      = false
-  vpc_cni_version     = "1.18.1"
-  subnets_filter_name = "general-subnet"      # subnets da rede CNI
-  sg_filter_name      = "eks-default-node" # sg do node
+  vpc_cni_version     = "1.19.6"
+  subnets_filter_name = "general-subnet-private"      # subnets da rede CNI
+  sg_filter_name      = "eks-cluster-node" # sg do node
 
   # aws_load_balancer_controller - https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
-  alb_controller_enable  = false
-  alb_controller_version = "1.7.2"
+  alb_controller_enable  = true
+  alb_controller_version = "1.13.3"
 
   # autoscaler - https://artifacthub.io/packages/helm/cluster-autoscaler/cluster-autoscaler
   autoscaler_enable  = false
-  autoscaler_version = "9.37.0"
+  autoscaler_version = "9.47.0"
 
   # metrics_server - https://artifacthub.io/packages/helm/metrics-server/metrics-server
   metrics_server_enable  = true
@@ -31,34 +31,34 @@ module "eks_core_apps" {
 
   # nginx_controler - https://artifacthub.io/packages/helm/ingress-nginx/ingress-nginx
   nginx_controler_enable  = true
-  certificate_arn         = "arn:aws:acm:..."
-  nginx_controler_version = "4.10.0"
+  #certificate_arn         = "arn:aws:acm:..."
+  nginx_controler_version = "4.13.0"
 
   # kube_dashboard - https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard
-  kube_dashboard_enable        = true
-  kube_dashboard_version       = "7.1.3"
+  kube_dashboard_enable        = false
+  kube_dashboard_version       = "7.13.0"
   kube_dashboard_ingress_class = "nginx"
   kube_dashboard_url           = "kubedashboard.dominio"
 
   # kubecost - https://artifacthub.io/packages/helm/kubecost/cost-analyzer
-  kubecost_enable        = false
-  kubecost_version       = "2.3.0"
+  kubecost_enable        = true
+  kubecost_version       = "2.8.0"
   kubecost_ingress_class = "nginx"
   kubecost_url           = "kubecost.dominio"
 
   # kube_prometheus_stack - https://artifacthub.io/packages/helm/prometheus-community/kube-prometheus-stack
-  kube_prometheus_stack_enable                  = true
-  kube_prometheus_stack_version                 = "68.1.0"
+  kube_prometheus_stack_enable                  = false
+  kube_prometheus_stack_version                 = "75.9.0"
   kube_prometheus_stack_grafana_ingress_class   = "nginx"
   kube_prometheus_stack_grafana_url             = "grafana.dominio"
   
   # aws_ebs_csi_driver - https://artifacthub.io/packages/helm/aws-ebs-csi-driver/aws-ebs-csi-driver
   ebs_csi_driver_enable      = true
-  ebs_csi_driver_version     = "2.39.3"
+  ebs_csi_driver_version     = "2.45.1"
   
   # external_dns - https://artifacthub.io/packages/helm/bitnami/external-dns
-  external_dns_enable              = true
-  external_dns_version             = "8.7.4"
+  external_dns_enable              = false
+  external_dns_version             = "8.9.1"
   external_dns_hosted_zone_id      = "Z06655772UAPK5LQZERAO"
   external_dns_hosted_zone_domain  = "dominio"
 
